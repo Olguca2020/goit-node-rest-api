d71e0ab5ae4670d9c880db6b3b36629ce0af0b54
Shchyrba Olga <olga.binder@gmail.com>
2024-04-15 21:35:56 +0300

    finish

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ import Joi from "joi";
 import bcrypt from "bcrypt";
 import { User } from "../models/user.js";
 import jwt from "jsonwebtoken";
+import authenticateToken from "../middleware/authenticateToken.js";
 
 const authRouter = express.Router();
 
@@ -65,12 +66,51 @@ authRouter.post("/login", async (req, res) => {
       expiresIn: "1h",
     });
 
+    user.token = token;
+    await user.save();
+
     res.status(200).json({
       token,
       user: {
         email: user.email,
         subscription: user.subscription,
       },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+authRouter.post("/logout", authenticateToken, async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+    user.token = null;
+    await user.save();
+    res.status(204).end();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+authRouter.get("/current", authenticateToken, async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+
+    res.status(200).json({
+      email: user.email,
+      subscription: user.subscription,
     });
   } catch (error) {
     console.error(error);
