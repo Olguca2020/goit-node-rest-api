ba9103af78a1803f169edf3f1b37c09e6b2c24df
Shchyrba Olga <olga.binder@gmail.com>
2024-04-24 18:48:31 +0300

    first

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,8 @@ import { User } from "../models/user.js";
 import jwt from "jsonwebtoken";
 import authenticateToken from "../middleware/authenticateToken.js";
 import { updateAvatar, uploadAvatar } from "../middleware/userMidleware.js";
+import { nanoid } from "nanoid";
+import nodemailer from "nodemailer";
 
 const authRouter = express.Router();
 
@@ -27,7 +29,7 @@ authRouter.post("/register", async (req, res) => {
     if (existingUser) {
       return res.status(409).json({ message: "Email in use" });
     }
-
+    const verificationToken = nanoid();
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const emailHash = crypto
       .createHash("md5")
@@ -38,14 +40,22 @@ authRouter.post("/register", async (req, res) => {
       email: req.body.email,
       password: hashedPassword,
       avatarURL: avatar,
+      verificationToken: verificationToken,
     });
     await newUser.save();
 
+    await sendVerificationEmail(
+      newUser.email,
+      verificationToken,
+      req.protocol,
+      req.get("host")
+    );
     res.status(201).json({
       user: {
         email: newUser.email,
         subscription: "starter",
         avatar: newUser.avatarURL,
+        verificationToken: newUser.verificationToken,
       },
     });
   } catch (error) {
@@ -150,5 +160,86 @@ authRouter.patch(
     }
   }
 );
+
+authRouter.get("/verify/:verificationToken", async (req, res) => {
+  try {
+    const { verificationToken } = req.params;
+    const user = await User.findOne({ verificationToken });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    user.verificationToken = null;
+    user.verify = true;
+    await user.save();
+
+    res.status(200).json({ message: "Verification successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+const sendVerificationEmail = async (
+  email,
+  verificationToken,
+  protocol,
+  host
+) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.meta.ua",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "olguca@meta.ua",
+        pass: process.env.META_PASSWORD,
+      },
+    });
+
+    const verificationLink = `${protocol}://${host}/users/verify/${verificationToken}`;
+
+    const mailOptions = {
+      from: "olguca@meta.ua",
+      to: email,
+      subject: "Email Verification",
+      html: `<p>Please click the following link to verify your email: <a href="${verificationLink}">${verificationLink}</a></p>`,
+    };
+
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error("Error sending verification email:", error);
+  }
+};
+
+authRouter.post("/verify", async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ message: "Missing required field email" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (user.verify) {
+      return res
+        .status(400)
+        .json({ message: "Verification has already been passed" });
+    }
+
+    await sendVerificationEmail(user.email, user.verificationToken);
+
+    res.status(200).json({ message: "Verification email sent" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
 
 export default authRouter;
